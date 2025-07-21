a7f08439a0437d51fa71b4ee22e42358a00a6d96
编程界的小学生 <15620646321@163.com>
2025-07-21 14:04:50 +0800

    Update model.py

--- wanvideo/modules/model.py
+++ wanvideo/modules/model.py
@@ -1621,30 +1621,34 @@ class WanModel(ModelMixin, ConfigMixin):
                 pred_id = self.teacache_state.new_prediction(cache_device=self.cache_device)
                 should_calc = True                
             else:
-                previous_modulated_input = self.teacache_state.get(pred_id)['previous_modulated_input']
-                previous_modulated_input = previous_modulated_input.to(device)
-                previous_residual = self.teacache_state.get(pred_id)['previous_residual']
-                accumulated_rel_l1_distance = self.teacache_state.get(pred_id)['accumulated_rel_l1_distance']
-
-                if self.teacache_use_coefficients:
-                    rescale_func = np.poly1d(self.teacache_coefficients[self.teacache_mode])
-                    temb = e if self.teacache_mode == 'e' else e0
-                    accumulated_rel_l1_distance += rescale_func((
-                        (temb.to(device) - previous_modulated_input).abs().mean() / previous_modulated_input.abs().mean()
-                        ).cpu().item())
-                    del temb
-                else:
-                    temb_relative_l1 = relative_l1_distance(previous_modulated_input, e0)
-                    accumulated_rel_l1_distance = accumulated_rel_l1_distance.to(e0.device) + temb_relative_l1
-                    del temb_relative_l1
-
-
-                if accumulated_rel_l1_distance < self.rel_l1_thresh:
-                    should_calc = False
-                else:
+                if not self.teacache_state.has_prediction(pred_id):
+                    pred_id = self.teacache_state.new_prediction(cache_device=self.cache_device)
                     should_calc = True
-                    accumulated_rel_l1_distance = torch.tensor(0.0, dtype=torch.float32, device=device)
-                accumulated_rel_l1_distance = accumulated_rel_l1_distance.to(self.cache_device)
+                else:
+                    previous_modulated_input = self.teacache_state.get(pred_id)['previous_modulated_input']
+                    previous_modulated_input = previous_modulated_input.to(device)
+                    previous_residual = self.teacache_state.get(pred_id)['previous_residual']
+                    accumulated_rel_l1_distance = self.teacache_state.get(pred_id)['accumulated_rel_l1_distance']
+    
+                    if self.teacache_use_coefficients:
+                        rescale_func = np.poly1d(self.teacache_coefficients[self.teacache_mode])
+                        temb = e if self.teacache_mode == 'e' else e0
+                        accumulated_rel_l1_distance += rescale_func((
+                            (temb.to(device) - previous_modulated_input).abs().mean() / previous_modulated_input.abs().mean()
+                            ).cpu().item())
+                        del temb
+                    else:
+                        temb_relative_l1 = relative_l1_distance(previous_modulated_input, e0)
+                        accumulated_rel_l1_distance = accumulated_rel_l1_distance.to(e0.device) + temb_relative_l1
+                        del temb_relative_l1
+    
+    
+                    if accumulated_rel_l1_distance < self.rel_l1_thresh:
+                        should_calc = False
+                    else:
+                        should_calc = True
+                        accumulated_rel_l1_distance = torch.tensor(0.0, dtype=torch.float32, device=device)
+                    accumulated_rel_l1_distance = accumulated_rel_l1_distance.to(self.cache_device)
 
             previous_modulated_input = e.to(self.cache_device).clone() if (self.teacache_use_coefficients and self.teacache_mode == 'e') else e0.to(self.cache_device).clone()
            
